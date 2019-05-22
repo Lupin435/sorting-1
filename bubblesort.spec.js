80d84bfa1c345c17eda285c9b8f7fc70d1204f6f
daphne178 <realta23dc@gmail.com>
2019-05-22 13:16:18 -0400

    testem

---
+++ bubblesort.spec.js
@@ -0,0 +1,30 @@
+
+
+
+describe('Bubble Sort', function(){
+  beforeAll(function () {
+    spyOn(window, 'swap').and.callThrough();
+  });
+
+  beforeEach(function(){
+    window.swap.calls.reset();
+  })
+
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+  it('handles one element array', function(){
+    expect( bubbleSort([1]) ).toEqual( [1] );
+  });
+  it('sorts an array', function(){
+    expect( bubbleSort([1,4,2,3]) ).toEqual( [1,2,3,4] );
+  });
+  it('sorts an array several times', function(){
+    expect( bubbleSort([4,3,2,1]) ).toEqual( [1,2,3,4] );
+    expect(
+      window.swap.calls.count()).toEqual(6);
+  });
+
+});
+
+console.log('WINDOW', window.swap)
